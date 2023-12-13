3ed848a2284b259e42bd7c68d52aae179c98d66e
Stevens Garcia <smalmsteeng@gmail.com>
2023-12-12 20:34:22 -0500

    :bento: Include lock account functionality

--- modules/bank_account.py
+++ modules/bank_account.py
@@ -10,7 +10,7 @@ class Bank_Account:
 
   def __init__(self, 
     balance = 0.0, 
-    status = "available", # available | locked | closed 
+    status = "active", # active | locked | closed 
     account_profile = "bronce", # bronce | silver | gold  | platinum
     currency = "USD") -> None:
     
@@ -19,37 +19,100 @@ class Bank_Account:
     self.account_id = Bank_Account.id
     
     # Create account meta-data
-    self.account_transactions = []
+    self.account_transactions_storage = []
     self.balance = Decimal(balance)
     self.status = status
     self.account_profile = account_profile
     self.currency = currency
 
-    Log("Create savings account", self.account_id, self.account_transactions)
+    Log("Create savings account", self.account_id, self.account_transactions_storage)
+
+  #############################################################################
+  # ACCOUNT HISTORY
+  #############################################################################
 
   # Get historical transactions
   def get_transaction_logs(self, how_many = 2) -> list:
-    Log("Get transaction logs", self.account_id, self.account_transactions)
-    return json.dumps(self.account_transactions[:-how_many], default=datetime_encoder, indent=2)
+    Log("Get transaction logs", self.account_id, self.account_transactions_storage)
+
+    def statement() -> list:
+      json_logs = json.dumps(self.account_transactions_storage[:-how_many], 
+        default=datetime_encoder, indent=2)
+      return json_logs
+    
+    return self.validate_account_status(statement)
   
   # Check current account's balance
   def check_balance(self) -> float:
-    Log("Check balance", self.account_id, self.account_transactions)
-    return Decimal(self.balance)
-  
-  # TODO: Lock account
-  def freeze_account(self) -> bool:
-    Log("Frezze account", self.account_id, self.account_transactions)
-    pass
+    Log("Check balance", self.account_id, self.account_transactions_storage)
 
-  # TODO: Transfer money to another account
-  def transfer_money(self) -> None:
-    Log("Transfer money", self.account_id, self.account_transactions)
-    pass
+    def statement():
+      return Decimal(self.balance)
+    
+    return self.validate_account_status(statement)
+
+  #############################################################################
+  # CHANGE ACCOUNT STATUS
+  #############################################################################
+
+  # Unlock/Lock savings account
+  def lock_account(self) -> str:
+    Log("Lock account", self.account_id, self.account_transactions_storage)
+    self.status = "locked"
+    return self.status
+  
+  def unlock_account(self) -> str:
+    Log("Unlock account", self.account_id, self.account_transactions_storage)
+
+    if not self.is_locked(): 
+      print(f"Your account is already {self.status.upper()}")
+    else:
+      self.status = "active"
+
+    return self.status
+
+  # Check account status: active | locked | closed
+  def check_account_status(self) -> str:
+    Log("Check account status", self.account_id, self.account_transactions_storage)
+    return self.status
+  
+  def is_locked(self) -> bool:
+    if self.status != "active":
+      return True
+    else:
+      return False
+
+  # Helper function to validate if the account is locked, so then the requested
+  # action can be perfomed
+  def validate_account_status(self, statement):
+    if not self.is_locked():
+      return statement()
+    else:
+      print("\n#################################################################")
+      print(f"Account is {self.status}. \nYou cann't deposit right now!. Please **ACTIVATE** your account first.")
+      print("#################################################################\n")
+      return False
+  
+  #############################################################################
+  # ACCOUNT MONEY TRANSACTIONS
+  #############################################################################
 
   # Add money to the savings account
   def deposit_money(self, amount) -> float:
-    Log("Deposit money", self.account_id, self.account_transactions)
-    self.balance += Decimal(amount)
-    return Decimal(self.balance)
-  
+    Log("Deposit money", self.account_id, self.account_transactions_storage)
+
+    def statement():
+      self.balance += Decimal(amount)
+      return self.balance
+    
+    self.validate_account_status(statement)
+      
+  # TODO: Transfer money to another account
+  # def transfer_money(self) -> None:
+  #   Log("Transfer money", self.account_id, self.account_transactions_storage)
+  #   pass
+
+  # TODO: Withrow money
+  # def withrow_money(self) -> None:
+  #   Log("Withrow money", self.account_id, self.account_transactions_storage)
+  #   pass
